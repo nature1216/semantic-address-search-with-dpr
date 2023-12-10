67749fefcd1126d31a66936464cdaa63fbecb5c9
nature1216 <annekim1216@naver.com>
2023-12-11 00:33:19 +0900

    feat: connect to db connect to db

--- app.py
+++ app.py
@@ -1,30 +1,53 @@
 from flask import Flask, request, jsonify
 from http import HTTPStatus
 from flask_cors import CORS
-from sentence_transformers import SentenceTransformer
+from sentence_transformers import SentenceTransformer, uti
 import json
 import numpy as np
+import pymysql
+
+def get_db_connection():
+    return pymysql.connect(host='', user='', password='', db='', charset='utf8')
 
 app = Flask(__name__)
 CORS(app)
-corpus_embeddings = np.load('/home/ubuntu/corpus_embeddings.npy')
-model = SentenceTransformer('/home/ubuntu/output')
+
+# model = SentenceTransformer()
+# corpus_embeddings = np.load()
 
 @app.route('/model', methods=['POST', 'OPTIONS'])
 def model():
-    query = ""
     if request.method == 'POST':
         data = request.get_json()
         query = data['query']
         hits = search_bi_encoder(query)
 
-    result = {'query': query}
+        db = get_db_connection()
+        cursor = db.cursor()
 
-    return jsonify(result)
+        try:
+            sql = "select * from coordinates where docid=" + str(hits[0]) + ";"
+            cursor.execute(sql)
+            rows = cursor.fetchall()
+
+            for row in rows:
+                docid = row[0]
+                x = row[1]
+                y = row[2]
+
+                if docid == hits[0]:
+                    result = {"x" : x, "y" : y, "status":HTTPStatus.OK}
+        finally:
+            cursor.close()
+            db.close()
+            return jsonify(result)
+    return '', 204
 
 def search_bi_encoder(query):
+    model = SentenceTransformer()
+    corpus_embeddings = np.load()
     question_embedding = model.encode(query, convert_to_tensor=True)
-    question_embedding = question_embedding.cuda()
+#    question_embedding = question_embedding.cuda()
     hits = util.semantic_search(question_embedding, corpus_embeddings, top_k=5)
     hits = hits[0]
     hits = sorted(hits, key=lambda x: x['score'], reverse=True)
