50e45d2e454eae8356add733eebcdb2f88b6d93d
nature1216 <annekim1216@naver.com>
2023-12-10 18:54:46 +0900

    feat: load model, corpus_embeddings load model, corpus_embeddings

--- app.py
+++ app.py
@@ -1,31 +1,36 @@
 from flask import Flask, request, jsonify
 from http import HTTPStatus
-# from sentence_transformers import SentenceTransformer
+from flask_cors import CORS
+from sentence_transformers import SentenceTransformer
 import json
 import numpy as np
 
 app = Flask(__name__)
-# corpus_embeddings = np.load('/home/ubuntu/corpus_embeddings.npy')
-# model = SentenceTransformer('/home/ubuntu/output')
+CORS(app)
+corpus_embeddings = np.load('/home/ubuntu/corpus_embeddings.npy')
+model = SentenceTransformer('/home/ubuntu/output')
 
-@app.route('/model', methods=['POST','GET'])
+@app.route('/model', methods=['POST', 'OPTIONS'])
 def model():
-    params = request.get_json()
-    query = params['query']
+    query = ""
+    if request.method == 'POST':
+        data = request.get_json()
+        query = data['query']
+        hits = search_bi_encoder(query)
 
+    result = {'query': query}
 
+    return jsonify(result)
 
-    return jsonify({"x": query, "y": query, "status": HTTPStatus.OK})
+def search_bi_encoder(query):
+    question_embedding = model.encode(query, convert_to_tensor=True)
+    question_embedding = question_embedding.cuda()
+    hits = util.semantic_search(question_embedding, corpus_embeddings, top_k=5)
+    hits = hits[0]
+    hits = sorted(hits, key=lambda x: x['score'], reverse=True)
+    hits = [hit['corpus_id'] for hit in hits[:5]]
 
-# def search_bi_encoder(query):
-#     question_embedding = model.encode(query, convert_to_tensor=True)
-#     question_embedding = question_embedding.cuda()
-#     hits = util.semantic_search(question_embedding, corpus_embeddings, top_k=5)
-#     hits = hits[0]
-#     hits = sorted(hits, key=lambda x: x['score'], reverse=True)
-#     hits = [hit['corpus_id'] for hit in hits[:5]]
-
-#     return hits
+    return hits
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
